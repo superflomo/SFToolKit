f0b97f3dfa6fa223df1f1c6d91ef2234994c0f29
Florian Buerger <flo@superflomo.com>
2013-04-01 17:05:54 +0200

    Move grimme TTYLogFormatter into SFToolKit

---
+++ SFToolKit/SFTTYLogFormatter.h
@@ -0,0 +1,12 @@
+//
+//  SFTTYLogFormatter.h
+//  SFToolKit
+//
+//  Created by Florian Bürger on 01.04.13.
+//  Copyright (c) 2013 superflomo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SFTTYLogFormatter : NSObject
+@end
