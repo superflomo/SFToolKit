2cb786a15a4782097d33690b6c7337990f50de81
Moritz Haarmann <m.haarmann@gmail.com>
2013-04-03 17:22:38 +0200

    + Added UIColor+RGB

---
+++ SFToolKit/UIColor+RGB.h
@@ -0,0 +1,16 @@
+//
+//  UIColor+RGB.h
+//  SFToolkit
+//
+//  Created by Moritz Haarmann on 08.08.12.
+//  Copyright (c) 2012 Moritz Haarmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (RGB)
++(UIColor*)colorWithHexValue:(uint)hexValue andAlpha:(float)alpha;
++(UIColor*)colorWithHexString:(NSString *)hexString andAlpha:(float)alpha;
++(UIColor*)colorWithHexString:(NSString *)hexString;
+
+@end
---
+++ SFToolKit/UIColor+RGB.m
@@ -0,0 +1,35 @@
+//
+//  UIColor+RGB.m
+//  SFToolkit
+//
+//  Created by Moritz Haarmann on 08.08.12.
+//  Copyright (c) 2012 Moritz Haarmann. All rights reserved.
+//
+
+#import "UIColor+RGB.h"
+
+@implementation UIColor (RGB)
+
++(UIColor*)colorWithHexValue:(uint)hexValue andAlpha:(float)alpha {
+    return [UIColor
+            colorWithRed:((float)((hexValue & 0xFF0000) >> 16))/255.0
+            green:((float)((hexValue & 0xFF00) >> 8))/255.0
+            blue:((float)(hexValue & 0xFF))/255.0
+            alpha:alpha];
+}
+
++(UIColor*)colorWithHexString:(NSString*)hexString andAlpha:(float)alpha {
+    UIColor *col;
+    hexString = [hexString stringByReplacingOccurrencesOfString:@"#"
+                                                     withString:@"0x"];
+    uint hexValue;
+    if ([[NSScanner scannerWithString:hexString] scanHexInt:&hexValue]) {
+        col = [self colorWithHexValue:hexValue andAlpha:alpha];
+    } else {
+        // invalid hex string
+        col = [self blackColor];
+    }
+    return col;
+}
+
+@end
